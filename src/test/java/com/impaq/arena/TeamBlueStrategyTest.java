71de9c40864e5b9b3ddbe1ad701d12b8fbc9b21b
jaroslaw.herod@cumulocity.com <jaro@localhost.localdomain>
2014-02-25 20:42:26 +0100

    refactored test after chages in Blue Team strategy

--- src/test/java/com/impaq/arena/TeamBlueStrategyTest.java
+++ src/test/java/com/impaq/arena/TeamBlueStrategyTest.java
@@ -4,21 +4,21 @@ import org.junit.Test;
 
 public class TeamBlueStrategyTest extends BaseStrategyTest {
 
-	@Override
-	public Strategy getStrategy() {
-		return new TeamBlueStrategy();
-	}
+    @Override
+    public Strategy getStrategy() {
+        return new TeamBlueStrategy();
+    }
 
-	@Test
-	public void second_round_if_oponent_builders_more_than_wizzards_kill_builders(){
-		//given
-			gameBoard.setOponentBuilders(8).setWizzards(2);
-		
-		//when
-			gameBoard.executeRound(2);
-		
-		//than
-			assertExpectedActions(addBuilders(), addBuilders(), spyBuilders(), killBuilders() );
-	}
+    @Test
+    public void inSecondRoundShoudSpyWarriorsAndIfGreaterThenTenShouldAddWizardsTwiceAndKillWarriors() {
+        //given
+        gameBoard.setOponentWarriors(11).setWizzards(2);
+
+        //when
+        gameBoard.executeRound(2);
+
+        //than
+        assertExpectedActions(spyWarriors(), addWizards(), addWizards(), killWarriors());
+    }
 
 }
