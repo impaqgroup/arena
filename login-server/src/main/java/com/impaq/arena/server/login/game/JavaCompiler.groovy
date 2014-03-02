fb6b9e1b1fe82eecad818c340333436f415d909c
Darek Kaczyński <kaczynskid@gmail.com>
2014-03-02 22:55:35 +0100

    Limited player classes ClassLoader

--- login-server/src/main/java/com/impaq/arena/server/login/game/JavaCompiler.groovy
+++ login-server/src/main/java/com/impaq/arena/server/login/game/JavaCompiler.groovy
@@ -2,8 +2,24 @@ package com.impaq.arena.server.login.game
 
 import org.abstractmeta.toolbox.compilation.compiler.JavaSourceCompiler
 import org.abstractmeta.toolbox.compilation.compiler.impl.JavaSourceCompilerImpl
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
 
 class JavaCompiler {
+
+    private final ResourceLoader resourceLoader
+
+    private final Resource playerApi
+
+    private final ClassLoader playerApiClassLoader
+
+    @Autowired
+    JavaCompiler(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader
+        this.playerApi = resourceLoader.getResource("classpath:resources/player-api.jar")
+        this.playerApiClassLoader = new URLClassLoader(playerApi.getURL())
+    }
 
     Class compile(String className, String code) {
         JavaSourceCompiler compiler = new JavaSourceCompilerImpl();
@@ -11,7 +27,7 @@ class JavaCompiler {
 
         unit.addJavaSource(className, code)
 
-        ClassLoader classLoader = compiler.compile(getClass().getClassLoader(), unit);
+        ClassLoader classLoader = compiler.compile(playerApiClassLoader, unit);
         return classLoader.loadClass(className);
     }
 }
--- login-server/src/test/java/com/impaq/arena/server/login/game/JavaCompilerTest.groovy
+++ login-server/src/test/java/com/impaq/arena/server/login/game/JavaCompilerTest.groovy
@@ -1,15 +1,17 @@
 package com.impaq.arena.server.login.game
 
-import spock.lang.Ignore
+import org.springframework.core.io.DefaultResourceLoader
+import org.springframework.core.io.ResourceLoader
 import spock.lang.Specification
 
-@Ignore
 class JavaCompilerTest extends Specification {
+
+    ResourceLoader resourceLoader = new DefaultResourceLoader()
 
     def "Should compile strategy class source"() {
         given:
-            JavaCompiler compiler = new JavaCompiler()
-            InputStream strategyCode = getClass().getClassLoader().getResourceAsStream("ExampleStrategy.java")
+            JavaCompiler compiler = new JavaCompiler(resourceLoader)
+            InputStream strategyCode = resourceLoader.getResource("classpath:ExampleStrategy.java").getInputStream()
         when:
             Class clazz = compiler.compile("ExampleStrategy", strategyCode.getText())
         then:
