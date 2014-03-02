9f3652e9ec42a60ca75de932ca03ffb93cde775b
Adam Wojszczyk <adam.wojszczyk@impaqgroup.com>
2014-03-02 22:51:31 +0100

    Corrects ExampleStrategy and ExampleStrategy_idle files used to test compilation in runtime

--- login-server/src/test/resources/ExampleStrategy.java
+++ login-server/src/test/resources/ExampleStrategy.java
@@ -19,15 +19,15 @@ public class ExampleStrategy implements PlayerStrategy {
     }
 
     private void play1(Game game) {
-        if (game.spyEnemyWizzardsCount() > 8) {
-            if (game.myWizzardsCount() < 4) {
-                game.recruitWizzards();
-                game.recruitWizzards();
+        if (game.spyEnemyWizardsCount() > 8) {
+            if (game.myWizardsCount() < 4) {
+                game.recruitWizards();
+                game.recruitWizards();
             } else {
                 game.recruitBuilders();
                 game.recruitBuilders();
             }
-            game.attackEnemyWizzards();
+            game.attackEnemyWizards();
         } else if (game.spyEnemyCastleHeight() > 30) {
             game.recruitWarriors();
             game.recruitWarriors();
@@ -41,9 +41,9 @@ public class ExampleStrategy implements PlayerStrategy {
 
     private void play2(Game game) {
         if (game.spyEnemyWarriorsCount() > 8) {
-            if (game.myWizzardsCount() < 4) {
-                game.recruitWizzards();
-                game.recruitWizzards();
+            if (game.myWizardsCount() < 4) {
+                game.recruitWizards();
+                game.recruitWizards();
             } else {
                 game.recruitBuilders();
                 game.recruitBuilders();
@@ -62,9 +62,9 @@ public class ExampleStrategy implements PlayerStrategy {
 
     private void play3(Game game) {
         if (game.spyEnemyBuildersCount() > 8) {
-            if (game.myWizzardsCount() < 4) {
-                game.recruitWizzards();
-                game.recruitWizzards();
+            if (game.myWizardsCount() < 4) {
+                game.recruitWizards();
+                game.recruitWizards();
             } else {
                 game.recruitBuilders();
                 game.recruitBuilders();
---
+++ login-server/src/test/resources/ExampleStrategy_idle.java
@@ -0,0 +1,11 @@
+import com.impaq.arena.api.Game;
+import com.impaq.arena.api.PlayerStrategy;
+
+public class ExampleStrategy implements PlayerStrategy {
+
+    @Override
+    public void playRound(Game game) {
+
+    }
+
+}
