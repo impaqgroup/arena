e98d6a48b231e48bf768584820c8350a697242cc
Darek Kaczyński <kaczynskid@gmail.com>
2014-04-24 11:43:05 +0200

    Workshop preparation

--- client/swing/src/main/java/com/impaq/arena/client/TeamRedStrategy.java
+++ client/swing/src/main/java/com/impaq/arena/client/TeamRedStrategy.java
@@ -7,14 +7,6 @@ public class TeamRedStrategy implements PlayerStrategy {
 
     @Override
     public void playRound(Game game) {
-        if (game.spyEnemyBuildersCount()> 10) {
-            game.recruitWizards();
-            game.recruitWizards();
-            game.attackEnemyBuilders();
-        } else {
-            game.recruitWarriors();
-            game.recruitWarriors();
-            game.attackEnemyCastle();
-        }
     }
+
 }
--- client/swing/src/test/java/com/impaq/arena/client/TeamBlueStrategyTest.java
+++
@@ -1,5 +0,0 @@
-package com.impaq.arena.client;
-
-public class TeamBlueStrategyTest {
-
-}
